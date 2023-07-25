261bb2f2a3fe7e9695eb9d43ded1adbf331ef273
tracyandesia <uitracers@gmail.com>
2023-07-24 23:09:06 -0700

    refactored precision to issue the correct negative value

--- get_precision.c
+++ get_precision.c
@@ -8,9 +8,9 @@
  *
  * Return: Precision.
  */
-int get_precision(const char *format, int *i, va_list list)
+int get_precision(const char *format, int *j, va_list list)
 {
-	int curr_i = *i + 1;
+	int curr_i = *j + 1;
 	int precision = -1;
 
 	if (format[curr_i] != '.')
@@ -35,7 +35,7 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*j = curr_i - 1;
 
 	return (precision);
 }
