43ffeb88d5f21ab198cd51c42af412eccbc28cab
tracyandesia <uitracers@gmail.com>
2023-07-25 08:05:07 -0700

    refactors

--- utils.c
+++ utils.c
@@ -22,18 +22,18 @@ int is_printable(char c)
  * @ascii_code: ASSCI CODE.
  * Return: Always 3
  */
-int append_hexa_code(char ascii_code, char buffer[], int j)
+int append_hexa_code(char ascii_code, char buffer[], int i)
 {
 	char map_to[] = "0123456789ABCDEF";
 	/* The hexa format code is always 2 digits long */
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
-	buffer[j++] = '\\';
-	buffer[j++] = 'x';
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
 
-	buffer[j++] = map_to[ascii_code / 16];
-	buffer[j] = map_to[ascii_code % 16];
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
 
 	return (3);
 }
